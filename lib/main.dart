bd82c436a241819d26371b07423ce33306cb991d
Mochamad Ridwan <106060496+mchridwan04@users.noreply.github.com>
2023-04-21 16:26:41 +0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mandiri1/profil.dart';
+import 'package:mandiri1/widgetVisible.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Mochamad Ridwan',
+    home: Beranda(),
+  ));
+}
+
+class Beranda extends StatelessWidget {
+  const Beranda ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: const Icon(Icons.home),
+        title: const Text('Tugas Mandiri 1', style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              // Logo
+              const SizedBox(height: 50),
+              const Icon(Icons.flutter_dash, size: 120, color: Colors.blue),
+
+              // Teks Pembuka
+              const SizedBox(height: 50),
+              const Text('Selamat Datang', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+
+              const SizedBox(height: 20),
+              const Text('Silakan pilih menu :', style: TextStyle(fontSize: 14)),
+
+              const SizedBox(height: 10),
+              ButtonTheme(
+                child: ElevatedButton.icon(
+                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const profil())); },
+                  icon: const Icon(Icons.person), label: const Text('Profil Saya'),
+                  style: ElevatedButton.styleFrom(minimumSize: const Size(300, 50)),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              ButtonTheme(
+                child: ElevatedButton.icon(
+                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const widgetVisible())); },
+                  icon: const Icon(Icons.widgets_rounded), label: const Text('Widget Visible'),
+                  style: ElevatedButton.styleFrom(minimumSize: const Size(300, 50), backgroundColor: Colors.green),
+                ),
+              ),
+            ]),
+        ) 
+      ),
+
+    );
+  }
+}
---
+++ lib/profil.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class profil extends StatelessWidget {
+  const profil ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () => Navigator.of(context).pop(), icon: const Icon(Icons.arrow_circle_left)),
+        title: const Text('Profil Saya', style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Images Profil
+            SizedBox(
+              width: 120,
+              height: 120,
+              child: ClipRRect(borderRadius: BorderRadius.circular(100), child: const Image(image: AssetImage('assets/images/profil.png'))),
+            ),
+            // name account email
+            const SizedBox(height: 10),
+            const Text('Mochamad Ridwan', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const Text('mchridwan24@gmail.com',style: TextStyle(fontSize: 14)),
+
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: FaIcon(FontAwesomeIcons.facebook), style: ElevatedButton.styleFrom(shape: const CircleBorder(), backgroundColor: Colors.blue)),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+            const Divider(color: Colors.grey),
+
+            Form(child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextFormField(
+                  initialValue: 'Mochamad Ridwan',
+                  decoration: const InputDecoration(label: Text('Nama Lengkap'), prefixIcon: Icon(Icons.person), border: OutlineInputBorder()),
+                ),
+
+                const SizedBox(height: 5),
+                TextFormField(
+                  initialValue: 'mchridwan24@gmail.com',
+                  decoration: const InputDecoration(label: Text('email'), prefixIcon: Icon(Icons.email), border: OutlineInputBorder()),
+                ),
+
+                const SizedBox(height: 5),
+                TextFormField(
+                  initialValue: 'Jl. Kudus Pati, Klaling Gajian RT 6 RW 5, Kec. Jekulo, Kabupaten Kudus, Jawa Tengah 59382',
+                  decoration: const InputDecoration(label: Text('Alamat'), prefixIcon: Icon(Icons.home), border: OutlineInputBorder()),
+                  maxLines: 4,
+                ),
+
+
+                const SizedBox(height: 10),
+                ButtonTheme(
+                  child: ElevatedButton.icon(
+                    onPressed: () { notification(context);},
+                    icon: const Icon(Icons.save),
+                    label: const Text('Simpan'),
+                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50), backgroundColor: Colors.green),
+                  ),
+                )
+              ],
+              ))
+          ],
+          ) 
+        ),
+    );
+  }
+}
+
+void notification(BuildContext context) {
+  showDialog(
+    context: context, 
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Notifikasi!'),
+        content: const Text('Simpan data berhasil!'),
+        actions: <Widget> [
+          ElevatedButton(
+            child: const Text ('Oke'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      );
+    }
+    );
+}
