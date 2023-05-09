7a32192850115112fc0a0f248331fe4b0a17cd63
Mochamad Ridwan <106060496+mchridwan04@users.noreply.github.com>
2023-05-09 14:24:51 +0700

    Tugas 3 Done

--- lib/halHWICliprrect.dart
+++ lib/halHWICliprrect.dart
@@ -6,24 +6,21 @@ class HalHWICliprrect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white,
         leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(), 
-          icon: const Icon(
-            Icons.arrow_circle_left,
-            color: Colors.blue
-          ),
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(Icons.arrow_circle_left, color: Colors.blue),
         ),
       ),
       body: Center(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(150),
-          child: Container(
-            height: 300,
-            width: 300,
-            color: Colors.deepPurple,
-          )
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+              child:
+                  const Image(image: AssetImage('assets/images/profil.png'))),
         ),
       ),
     );
--- lib/halHWIGridview.dart
+++ lib/halHWIGridview.dart
@@ -10,108 +10,118 @@ class HalHWIGridview extends StatelessWidget {
         backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(
-            Icons.arrow_circle_left,
-            color: Colors.blue
-          ),
+          icon: const Icon(Icons.arrow_circle_left, color: Colors.blue),
         ),
-        title: const Text('GridView', 
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.blue
-        ),),
+        title: const Text(
+          'GridView',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
       ),
       body: GridView.count(
         crossAxisCount: 3,
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1683143724719-2dc07dc1fe31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://plus.unsplash.com/premium_photo-1664368276702-978a7b61a413?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1683524430079-d45876f50fed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1683122397672-14c6ff3eb008?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1683294163345-1cd8cd02147f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5OXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50, 
-              width: 50, 
-              color: Colors.deepPurple
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1682687982141-0143020ed57a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+                      ),
+                    ),
+                  ),
+                ),
+              )),
         ],
       ),
     );
