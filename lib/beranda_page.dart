26e2f3f45eff13b9f86e45dd735927c1c557255e
Mochamad Ridwan <106060496+mchridwan04@users.noreply.github.com>
2023-06-19 23:25:32 +0700

    done

---
+++ lib/beranda_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class BerandaPage extends StatelessWidget {
+  const BerandaPage({super.key});
+
+  final double horizontalPadding = 40;
+  final double vertikalPadding = 25;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: horizontalPadding, vertical: vertikalPadding),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(Icons.home, color: Colors.black87, size: 65),
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.cyan,
+                        child: Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ClipOval(
+                            child: Image.network(
+                                'https://cdn.icon-icons.com/icons2/2468/PNG/512/user_icon_149329.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      child: Text(
+                        'Mochamad Ridwan',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+
+        // !!! Teks Information
+        const SizedBox(height: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text('Mochamad Ridwan',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text(
+              'Menu Aplikasi',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+            )
+          ],
+        ),
+
+        // !!! Line
+        const SizedBox(height: 20),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+          child: const Divider(
+            color: Colors.red,
+            thickness: 2,
+          ),
+        ),
+
+        // !!! Buttons
+        Expanded(
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              ButtonTheme(
+                  child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+                label: const Text('Beranda'),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(300, 65),
+                    backgroundColor: Colors.indigo),
+              )),
+              const SizedBox(height: 10),
+              ButtonTheme(
+                  child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+                label: const Text('Beranda'),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(300, 65),
+                    backgroundColor: Colors.indigo),
+              )),
+            ],
+          ),
+        ))
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:mandiri1/beranda.dart';
+import 'package:mandiri1/beranda_page.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: 'APP UAS MP 202053046',
+    title: 'Ujian Akhir Semester',
     home: FormMasuk(),
   ));
 }
 
 class FormMasuk extends StatelessWidget {
   const FormMasuk({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Aplikasi UAS',
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.teal,
+        title: const Text(
+          'Aplikasi Ujian Akhir Semester',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      backgroundColor: Colors.grey[100],
+      backgroundColor: Colors.deepOrange[200],
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            children: [
-              //logo atau icon
-              const SizedBox(height: 50),
-              const Icon(
-                Icons.person_pin,
-                size: 175,
-                color: Colors.teal,
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(children: [
+              // !!! Logo or icons
+              const SizedBox(
+                height: 50,
               ),
-              //teks pembuka
-              const SizedBox(height: 35),
-              const Text('Selamat datang',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 15),
-              const Text('Silahkan masukkan :', style: TextStyle(fontSize: 14)),
-              //kolom inputan & tombol masuk
+              const Icon(Icons.person, size: 175, color: Colors.blue),
+
+              // !!!  Teks Pembuka
+              const SizedBox(height: 50),
+              const Text(
+                'Selamat Datang',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 50),
+              const Text('Silahkan Masuk', style: TextStyle(fontSize: 14)),
+
+              // !!! Form input username
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
-                    label: Text('Username'),
+                    label: Text('username'),
                     prefixIcon: Icon(Icons.person),
                     border: OutlineInputBorder()),
               ),
+
+              // !!! Form input passowrd
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
-                    label: Text('Password'),
+                    label: Text('password'),
                     prefixIcon: Icon(Icons.lock),
                     border: OutlineInputBorder()),
               ),
-              const SizedBox(height: 15),
+
+              // !!! Button
+              const SizedBox(height: 10),
               ButtonTheme(
-                child: ElevatedButton.icon(
-                  onPressed: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const beranda())),
-                  icon: const Icon(Icons.login),
-                  label: const Text('M a s u k'),
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(65)),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+                  child: ElevatedButton.icon(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BerandaPage())),
+                icon: const Icon(Icons.login),
+                label: const Text('Masuk'),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(65)),
+              ))
+            ]),
+          )),
     );
   }
 }
